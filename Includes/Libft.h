5cdaa99ba08d9d750097b67f49ae3e329d582d92
Alex <alex@M.local>
2025-09-21 13:31:14 +0200

    v1.3

--- Includes/Libft.h
+++ Includes/Libft.h
@@ -65,25 +65,40 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 
+
+
 // 	BONUS  // 
 typedef struct s_list
 {
 	void			*content;
 	struct s_list	*next;
-
 }	t_list;
 
+// Create new node
+t_list	*ft_lstnew(void *content);
 
+// Add node at the beginning
+void	ft_lstadd_front(t_list **alst, t_list *new);
+
+// Add node at the end
+void	ft_lstadd_back(t_list **lst, t_list *new);
+
+// Return last node
+t_list	*ft_lstlast(t_list *lst);
+
+// Count nodes in list
 int		ft_lstsize(t_list *lst);
 
-void	ft_lstadd_front(t_list **alst, t_list *new);
-void	ft_lstadd_back(t_list **lst, t_list *new);
+// Delete one node
 void	ft_lstdelone(t_list *lst, void (*del)(void *));
+
+// Delete and free entire list
 void	ft_lstclear(t_list **lst, void (*del)(void *));
+
+// Apply function to each node
 void	ft_lstiter(t_list *lst, void (*f)(void*));
 
-t_list	*ft_lstnew(void *content);
-t_list	*ft_lstlast(t_list *lst);
+// Map list into a new one
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void*), void (*del)(void *));
 
 #endif
